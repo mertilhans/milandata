07e8e96ae3674d6fc8ccd27bb2c17fadfc41ea93
Hüseyin Sarpkaya <sarpkaya.hsyn@gmail.com>
2025-08-09 19:14:15 +0300

    Add files via upload

---
+++ sarpkaya/exit_status.c
@@ -0,0 +1,18 @@
+
+#include "shell.h"
+
+static int  *get_exit_status_adress(void)
+{
+    static int last_exit_status = 0;
+    return(&last_exit_status);
+}
+//int get_last_exit_status(void)
+//{
+//    int *status_ptr = get_exit_status_adress();
+//    return(*status_ptr);
+//}
+//void    set_last_exit_status(int status)
+//{
+//    int *status_ptr = get_exit_status_adress();
+//        *status_ptr = status;
+//}
